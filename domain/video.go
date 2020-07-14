81255558d643b039751baf2fe1ac18a7b798f651
netoudi <netoudi@rocketmail.com>
2020-07-14 09:21:46 -0300

    Configurando entidades

--- domain/video.go
+++ domain/video.go
@@ -10,11 +10,12 @@ func init() {
 }
 
 type Video struct {
-    ID         string    `valid:"uuid"`
-    ResourceID string    `valid:"notnull"`
-    FilePath   string    `valid:"notnull"`
-    CreatedAt  time.Time `valid:"-"`
-    UpdatedAt  time.Time `valid:"-"`
+    ID         string    `json:"encoded_video_folder" valid:"uuid" gorm:"type:uuid;primary_key"`
+    ResourceID string    `json:"resource_id" valid:"notnull" gorm:"type:varchar(255)"`
+    FilePath   string    `json:"file_path" valid:"notnull" gorm:"type:varchar(255)"`
+    CreatedAt  time.Time `json:"created_at" valid:"-"`
+    UpdatedAt  time.Time `json:"updated_at" valid:"-"`
+    Jobs       []*Job    `json:"-" valid:"-" gorm:"ForeignKey:VideoID"`
 }
 
 func NewVideo() *Video {
